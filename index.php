1a64815696cd2351ea2918315505fde9539d617b
md20242001 <md20242001@student.fon.bg.ac.rs>
2026-06-01 16:21:16 +0200

    Dodavanje h2 i h3

--- index.php
+++ index.php
@@ -29,7 +29,8 @@ $currentDate = date("F j, Y");
 
 <main>
     <section class="card">
-        <h2>Hello!</h2>
+        <h2>Hi there!</h2>
+        <h3>Welcome to my website</h3>
         <p>
             This page was generated using PHP.
         </p>
